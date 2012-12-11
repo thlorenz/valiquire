2fa6a4c5d54de5259ea4f64b09daa345b0f46a2c
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-11 07:55:25 -0500

    renaming things

--- test/valiquire.js
+++ test/valiquire.js
@@ -1,12 +1,12 @@
 'use strict';
 /*jshint asi:true */
 var test = require('tap').test
-  , jic = require('..')
+  , valiquire = require('..')
   , path = require('path')
 
 test('given ./deps/uno.js ./deps/dos.js ./deps/tres.js', function (t) {
-  t.test('when a module requires ./deps/Uno.js ./deps/dos.js ./deps/cuatro.js', function (t) {
-    var errors = jic(path.join(__dirname, 'fixtures/one-file'))
+  t.test('when two modules require ../deps/Uno.js ../deps/dos.js ../deps/cuatro.js', function (t) {
+    var errors = valiquire(path.join(__dirname, 'sample'))
     t.equals(errors.length, 2, 'finds two errors')
   })  
 })
